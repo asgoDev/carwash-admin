a101802fa6d98708e016ab47c0263517dca63c7e
asgodev <asgodev@gmail.com>
2025-09-21 11:28:48 -0400

    ClientDetail: table updated

--- src/pages/ClientDetail.jsx
+++ src/pages/ClientDetail.jsx
@@ -56,7 +56,7 @@ export default function ClientDetail() {
 
   return (
     <ContentLayout title={"Informacion general"}>
-      <form className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2 overflow-x-auto ">
         <label>
           <span className="font-bold">C. I.: </span>
           <input
@@ -106,43 +106,47 @@ export default function ClientDetail() {
           />
         </label>
 
-        <label>
-          <span className="font-bold">Vehículos: </span>
-          {/* <input
-            name="vehicleType"
-            value={client.vehicleType || ""}
-            onChange={handleChange}
-            disabled={isDisabled}
-          /> */}
+        <span className="font-bold">Vehículos: </span>
 
-          <table>
-            <thead>
+        <div className="max-w-[85%] sm:max-w-full overflow-hidden">
+          <table className=" mt-2 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 shadow-md sm:rounded-lg sm:rounded-lg overflow-hidden">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-900 dark:text-gray-400">
               <tr>
-                <td>n</td>
-                <td>Tipo</td>
-                <td>Marca</td>
-                <td>Modelo</td>
-                <td>Placa</td>
+                <th scope="col" className="px-2 py-3 md:px-6">
+                  #
+                </th>
+                <th scope="col" className="px-2 py-3 md:px-6">
+                  Tipo
+                </th>
+                <th scope="col" className="px-2 py-3 md:px-6">
+                  Marca
+                </th>
+                <th scope="col" className="px-2 py-3 md:px-6">
+                  Modelo
+                </th>
+                <th scope="col" className="px-2 py-3 md:px-6">
+                  Placa
+                </th>
               </tr>
             </thead>
-            {client.vehicles.map((vehicle, i) => (
-              <tbody>
-                <tr>
-                  <td>{i}</td>
-                  <td>{vehicle.type}</td>
-                  <td>{vehicle.brand}</td>
-                  <td>{vehicle.model}</td>
-                  <td>{vehicle.plate}</td>
+            <tbody>
+              {client.vehicles.map((vehicle, i) => (
+                <tr
+                  key={vehicle.plate}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-600 hover:bg-gray-50 dark:hover:bg-gray-700"
+                >
+                  <td className="px-2 py-4 md:px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {i + 1}
+                  </td>
+                  <td className="px-2 py-4 md:px-6">{vehicle.type}</td>
+                  <td className="px-2 py-4 md:px-6">{vehicle.brand}</td>
+                  <td className="px-2 py-4 md:px-6">{vehicle.model}</td>
+                  <td className="px-2 py-4 md:px-6">{vehicle.plate}</td>
                 </tr>
-              </tbody>
-            ))}
+              ))}
+            </tbody>
           </table>
-        </label>
-
-        {/* <button type="button" onClick={handleSave}>
-          Save
-        </button> */}
-
+        </div>
         <button onClick={editMode}>editar</button>
       </form>
     </ContentLayout>
