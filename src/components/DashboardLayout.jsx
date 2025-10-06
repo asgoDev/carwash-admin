5cc0b71e99701206254c91343b0ae9805d004c5a
asgodev <asgodev@gmail.com>
2025-10-06 13:02:14 -0400

    DashboarLayout created

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -9,12 +9,6 @@ const DashboardLayout = () => {
     <div className="grid grid-cols-[1fr_1fr] md:grid-cols-[1fr_2fr] gap-4">
       <div className="grid grid-cols-[repeat(auto-fit,minmax(160px,1fr))] gap-4 ">
         <article className="aspect-[1.6/1] bg-white border-b-5 border-gray-300 rounded-lg hover:border-blue-500 hover:scale-[1.01] transition-all duration-100 cursor-pointer shadow-md overflow-hidden flex flex-col ">
-          <header className="bg-gray-300 px-4 py-3">Clientes</header>
-          <div className="flex-grow-1 flex flex-col justify-center items-center">
-            <span className="font-bold text-2xl">{clients.length}</span>
-          </div>
-        </article>
-        <article className="aspect-[1.6/1] bg-white border-b-5 border-gray-300 rounded-lg hover:border-blue-500 hover:scale-[1.01] transition-all duration-100 cursor-pointer shadow-md overflow-hidden flex flex-col ">
           <header className="bg-gray-300 px-4 py-3">Tasa BCV</header>
           <div className="flex-grow-1 flex flex-col justify-center items-center">
             <span className="font-bold text-2xl">{rate}</span>
@@ -24,6 +18,12 @@ const DashboardLayout = () => {
           <header className="bg-gray-300 px-4 py-3">Clientes</header>
           <div className="flex-grow-1 flex flex-col justify-center items-center">
             <span className="font-bold text-2xl">{clients.length}</span>
+          </div>
+        </article>
+        <article className="aspect-[1.6/1] bg-white border-b-5 border-gray-300 rounded-lg hover:border-blue-500 hover:scale-[1.01] transition-all duration-100 cursor-pointer shadow-md overflow-hidden flex flex-col ">
+          <header className="bg-gray-300 px-4 py-3">Empleados</header>
+          <div className="flex-grow-1 flex flex-col justify-center items-center">
+            <span className="font-bold text-2xl">{employees.length}</span>
           </div>
         </article>
         <article className="aspect-[1.6/1] bg-white border-b-5 border-gray-300 rounded-lg hover:border-blue-500 hover:scale-[1.01] transition-all duration-100 cursor-pointer shadow-md overflow-hidden flex flex-col ">
